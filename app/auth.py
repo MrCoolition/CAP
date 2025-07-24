f32b920795dc72a9e1bbfc564a08f3d55ef1c760
MrCoolition <coolitioncraft@gmail.com>
2025-07-24 09:33:46 -0400

    Remove Auth0 env vars

--- app/auth.py
+++ app/auth.py
@@ -1,9 +1,12 @@
-import os
 import streamlit as st
 from auth0_component import login_button
 
-AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
-AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+AUTH0_CLIENT_ID = None
+AUTH0_DOMAIN = None
+
+if "auth0" in st.secrets:
+    AUTH0_CLIENT_ID = st.secrets["auth0"].get("client_id")
+    AUTH0_DOMAIN = st.secrets["auth0"].get("domain")
 
 
 def authenticate():
